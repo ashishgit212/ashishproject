c8056d3d94fc992f97acfa6c146fd540d74af56a
ashishgit212 <ashishshingareass@gmail.com>
2026-01-28 17:53:18 +0530

    Update db.php

--- enquiry_project/db.php
+++ enquiry_project/db.php
@@ -1,13 +1,16 @@
 <?php
+// db.php
+
 $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
-$port = getenv("DB_PORT");
+$port = getenv("DB_PORT") ?: 3306;
 
+// Create connection
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
+// Check connection
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-?>
